2b4c9cd478794f52bf401aedcf95b49e0ce761f8
Krzysiek (Chris) <kukizk@gmail.com>
2020-05-13 19:36:25 +0200

    Podmiana python_env 1/2

--- python_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/icookiez/Documents/praca/dla taty/poll/python_env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
